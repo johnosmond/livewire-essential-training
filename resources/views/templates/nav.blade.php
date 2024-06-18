7c8287ff7ba7feb982f4d3dd649803fa87814465
John Osmond <john@jtech10.com>
2024-06-18 10:31:39 -0400

    creates first component

---
+++ resources/views/templates/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+    <a href="/" wire:navigate
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+    <a href="about" wire:navigate
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 pl-4">About</a>
+    <a href="dashboard" wire:navigate
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 pl-4">Dashboard</a>
+</div>
